59b6966c78761a91425015c9493cc86f5a1ec60f
~parksong2~ <~ssong2.1019@gmail.com~>
2020-02-11 22:59:29 +0900

    200211 main ScrollView league_info ImageView, ViewPager

--- app/src/main/java/com/ezen/lolketing/MainActivity.java
+++ app/src/main/java/com/ezen/lolketing/MainActivity.java
@@ -6,26 +6,27 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.View;
-import android.widget.Button;
-import android.widget.ImageButton;
 import android.widget.ImageView;
 import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
+
+    ImageView icon_info;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        ImageView icon_info = findViewById(R.id.icon_info);
+        icon_info = findViewById(R.id.icon_info);
         icon_info.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                Log.e("icon_info", "리그 정보 클릭");
-                Toast.makeText(MainActivity.this, "리그 정보 클릭", Toast.LENGTH_SHORT).show();
-                Intent intent = new Intent(getApplicationContext(), LeagueInfoActivity.class);
-                startActivity(intent);
+                Log.e("MainActivity", "리그정보 클릭됨");
+                Toast.makeText(getApplicationContext(),
+                        "리그정보 선택됨", Toast.LENGTH_SHORT).show();
+                Intent leagueIntent = new Intent(getApplicationContext(), LeagueInfoActivity.class);
+                startActivity(leagueIntent);
             }
         });
     }
